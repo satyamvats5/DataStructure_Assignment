f39b2620b4bacaad1cf9409a934f83a2413bb407
SATYAM KUMAR <satyamvats5@gmail.com>
2017-09-01 12:01:06 +0530

    Lab_No._2
    2:- TO convert a sparse matrix to 3 tupple
      form and perform some operations on this three
      tupple form.
    
    1:- To find the address of any element in an array
       using row order and column order both.
    
    Signed-off-by: SATYAM KUMAR <satyamvats5@gmail.com>

---
+++ 3.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{	
+	while(1) {
+		int row,col,temp;
+	
+		printf("Enter the upper limit of row:- ");
+		scanf("%d", &row);
+	
+		printf("Enter the upper limit of col:- ");	
+		scanf("%d", &col);
+	
+		int ARR[row][col];
+		int rowpos,colpos;
+	
+		printf("Enter the row number and column number respectively :- ");
+		scanf("%d %d", &rowpos,&colpos);
+	
+		unsigned int val1 = (unsigned int)ARR;    //Base Address of the array.
+	
+		printf("base address = %u\n",val1); //Printing the base address.
+		printf("Address of your desird position in row order = ");
+
+		int row_add, col_add;
+		row_add = val1 + (col * rowpos + colpos)*sizeof(int);    // Calculation of adress in row order.
+		printf("%u\n" ,row_add);            //Printing of address in row order.
+		printf("\n");
+	
+		printf("Address of your desird position in column order = ");
+		col_add = val1 + (row * colpos + rowpos)*sizeof(int); 		//calculation of address in column order.
+		printf("%u\n\n" ,col_add);		// Printing of address in column order.
+		
+		printf("\tTo know more address enter :-1 \n \tPress 0 to exit ");
+		scanf("%d",&temp);
+		if(temp == 0) {
+			return 0;
+		}
+	}
+}	
+
--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 array: aray.c
 	$(CC) aray.c -o array -Wall -Wextra -pedantic -std=c99
+address: 3.c
+	$(CC) 3.c -o address -Wall -Wextra -pedantic -std=c99
+sparse: 4.c
+	$(CC) 4.c -o sparse -Wall -Wextra -pedantic -std=c99
+
+
